81df0f48ea5496429c0ea09998f5e803f60a0844
jspmern <134581836+jspmern@users.noreply.github.com>
2023-12-11 18:57:32 +0530

    here

---
+++ config/cloudinary.js
@@ -0,0 +1,11 @@
+// import {v2 as cloudinary} from 'cloudinary';
+          
+// cloudinary.config({ 
+//   cloud_name: 'dhf9enpmd', 
+//   api_key: '142836668494335', 
+//   api_secret: '8K2JUInph2i9UgxAGQDORmp7kEo' 
+// });
+
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
