c296fe88252f37b964b4d393a6965510ec53d75f
jspmern <134581836+jspmern@users.noreply.github.com>
2023-12-11 14:05:28 +0530

    this is for env file

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,9 @@
 let mongoose=require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/LOGINANDREGI").then(()=>{
-     console.log('db is connected')
+let path=require('path')
+const dotenvPath = path.join(__dirname, '../.env');
+require('dotenv').config({ path: dotenvPath });
+mongoose.connect(`mongodb://127.0.0.1:${process.env.DB_PORT}/${process.env.DB_NAME}`).then(()=>{
+     console.log(`db connected`)
 }).catch((err)=>{
     console.log('db is not connected')
 })
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 let express = require("express");
+ 
 let path = require("path");
-let loginRoute=require('./route/loginRoute')
+let loginRoute = require("./route/loginRoute");
 let app = express();
-let PORT = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname,"public")));
+const dotenvPath = path.resolve(__dirname, '../.env');
+require('dotenv').config({ path: dotenvPath });
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 //this is for the connection of db
 require("../db/conn");
 //this is for route
-app.use(loginRoute)
+app.use(loginRoute);
 //this is for port
-app.listen(PORT, () => {
-  console.log(`your server is sart at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`your server is sart at http://localhost:${process.env.PORT}`);
 });
